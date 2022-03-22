b246f4417b4f3fd854b5b79f13b047fcd4db1e74
Chimoha31 <mokochii1108@gmail.com>
2022-03-21 22:04:23 -0700

    middleware

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = express();
 const userRouter = require("./routes/user");
 const PORT = 3000;
 
+// ミドルウェアを使っていくには、一番上で宣言しないと意図した動きにならない。
+app.use(mylogger);
+
+
 // HTMLやcssのような静的なファイルを表示させるには、app.use(espress.static(フォルダ名))を使用し、フォルダ名(仮)publicの中にindex.htmlを作成。
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // 又、テンプレートエンジンと呼ばれているexpress専用のものを使う事によって、動的なファイル(dataベースを読み込ませたり)も使う事ができる。
 app.set("view engine", "ejs");
@@ -18,12 +22,22 @@ app.get("/", (req, res) => {
   // res.sendStatus(500);
   // res.sendStatus(404); 
   // res.status(500).send("Error")
-  res.status(500).json({msg: "This is error"});
-});
+  // res.status(500).json({msg: "This is error"});
 
+  //テンプレートエンジンと呼ばれているexpress専用
+  res.render("index", {text: "Node.js and Express"});
+});
 // ルーティング。
 // app.use()を使用することで、エンドポイント(=/user)は共通として使い、その後のエンドポイントについてはuserRouterに任せるというルーティング設計をする事ができる。
 app.use("/user", userRouter);
+
+
+// ミドルウェア
+// 第三引数にnextを記述。ミドルウェアはサイクルを止めてはいけない。なのでnext()を記述。
+function mylogger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 
 // expressを使ってlocalサーバーを立ち上げる。その為にはlisten()という関数を使用
