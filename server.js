08f0b105a7977d1187027cbd380cef2315759869
Chimoha31 <mokochii1108@gmail.com>
2022-03-21 21:30:05 -0700

    Installed ejs

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 const userRouter = require("./routes/user");
 const PORT = 3000;
 
+// HTMLやcssのような静的なファイルを表示させるには、app.use(espress.static(フォルダ名))を使用し、フォルダ名(仮)publicの中にindex.htmlを作成。
+app.use(express.static("public"));
+
+// 又、テンプレートエンジンと呼ばれているexpress専用のものを使う事によって、動的なファイル(dataベースを読み込ませたり)も使う事ができる。
+app.set("view engine", "ejs");
+
 // get(), post(), patch(), delete()を使っていく
 app.get("/", (req, res) => {
   // console.log("Hello express2");
